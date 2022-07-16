b31a4aff50616b7d43595422b589f65b7fb8fa42
Aaron Kwame Andoh <77841074+AaronAndoh@users.noreply.github.com>
2022-07-16 23:27:16 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,130 @@
+<?php
+	// landing/index page
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with Pigga landing page.">
+    <meta name="author" content="Devcrud">
+    <title>Pigga | Free Bootstrap 4.3.x template</title>
+    <!-- font icons -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- Bootstrap + Pigga main styles -->
+	<link rel="stylesheet" href="css/pigga.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    
+    <!-- First Navigation -->
+    <nav class="navbar nav-first navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="images/nn.png" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Pigga Landing page">
+            </a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-primary" href="#home">CALL US : <span class="pl-2 text-muted">(+233) 54 142 3205</span></a>
+                </li>                   
+            </ul>
+        </div>
+    </nav>
+    <!-- End of First Navigation --> 
+    <!-- Second Navigation -->
+    <nav class="nav-second navbar custom-navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
+        <div class="container">
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto"> 
+                    <li class="nav-item">
+                    </li>
+                    <li class="nav-item">
+                    </li>
+                    <li class="nav-item">
+                    </li>
+                    <li class="nav-item">
+                    </li>
+                </ul> 
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Of Second Navigation --> 
+    <!-- Page Header -->
+    <header class="header">
+        <div class="overlay">
+            <img src="images/nn.png" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Pigga Landing page" class="logo">
+            <h1 class="subtitle">Nhyira Hotel</h1>
+            <h1 class="title">Be Part of the &amp; Blessing</h1> 
+            <a class="btn btn-primary mt-3" href="view/welcome_page.php">Book A Room</a> 
+			<a class="btn btn-primary mt-3" href="login/login.php">Login</a> 
+			<a class="btn btn-primary mt-3" href="login/register.php">Sign Up</a> 
+        </div>      
+    </header>
+    <!-- End Of Page Header --> 
+    <!-- About Section -->
+    <section id="about">
+        <div class="container">
+            <div class="row align-items-center">                
+                <div class="col-md-6">
+                    <h6 class="section-subtitle">Opening Times</h6>
+                    <h3 class="section-title">Working Times</h3>
+                    <p class="mb-1 font-weight-bold">Monday - Thursday : <span class="font-weight-normal pl-2 text-muted">7:00 am - 12:00 pm</span></p>
+                    <p class="mb-1 font-weight-bold">Friday - Saturday : <span class="font-weight-normal pl-2 text-muted">7:00 am - Midnight</span></p>
+                    <p class="mb-1 font-weight-bold">Saturday - Sunday : <span class="font-weight-normal pl-2 text-muted">9:00 am - 12:00 pm</span></p>
+
+                    <a href="view/show_rooms.php" class="btn btn-primary btn-sm w-md mt-4">Book a Room</a>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col">
+                            <img alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Pigga Landing page" src="images/54.jpeg" class="w-100 rounded shadow">
+                        </div>
+                        <div class="col">
+                            <img alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Pigga Landing page" src="images/f7.jpeg" class="w-100 rounded shadow">
+                        </div>
+                    </div>                  
+                </div>
+            </div>
+            <div class="section-devider my-6 transparent"></div>
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                </div>
+                <div class="col-md-6 order-1 order-sm-first">
+                    <div class="row">
+                        <div class="col">
+                            <img alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Pigga Landing page" src="images/20.jpeg" class="w-100 rounded shadow">
+                        </div>
+                        <div class="col">
+                            <img alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Pigga Landing page" src="images/hhk.jpeg" class="w-100 rounded shadow">
+                        </div>
+                    </div>                  
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End OF About Section -->
+
+    
+
+
+
+    <!-- core  -->
+    <script src="../s/jquery-3.4.1.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap affix -->
+    <script src="../js/bootstrap.affix.js"></script>
+
+    <!-- Pigga js -->
+    <script src="../js/pigga.js"></script>
+
+</body>
+</html>
+
+	
